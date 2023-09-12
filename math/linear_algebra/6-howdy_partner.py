88656a7be69471bc5c10712acf172f2f3ab6c765
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-09-12 17:22:57 +0200

    howdy partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from 6-howdy_partner import cat_arrays  # Import the cat_arrays function
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8]
+print(cat_arrays(arr1, arr2))  # Output: [1, 2, 3, 4, 5, 6, 7, 8]
+print(arr1)  # Output: [1, 2, 3, 4, 5]
+print(arr2)  # Output: [6, 7, 8]
+
