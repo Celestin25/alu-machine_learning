1753bf636b85ad137ebf19873c36fa41e69c4c34
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-09-23 18:16:15 +0200

    sum

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
 """
-Calculate the sum of squared values
+Summation of squared values
 """
 
 
-def calculate_sum_of_squares(n):
-    """ Calculate the sum of squares
+def summation_i_squared(n):
+    """ Sum i squared
 
     Args:
-        n (int): The number up to which the sum of squares should be calculated.
-        
-    Returns:
-        int or None: The sum of squares up to 'n' if 'n' is a positive integer, or None otherwise.
+        n (_type_): _description_
     """
     if n == 1:
         return 1
     if n < 1:
         return None
     else:
-        result = (n * (n + 1) * (2 * n + 1)) // 6
+        result = (n*(n+1)*(2*n+1))//6
         return result
 
-# Example usage:
-# print(calculate_sum_of_squares(5))
 
+# print(summation_i_squared(5))
