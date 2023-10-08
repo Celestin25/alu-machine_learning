f726c7a7d0047bb4d0b5bb4187a57a709f4f36d0
Celestin HAKORIMANA <celestin@DESKTOP-L0GB8TB.localdomain>
2023-10-08 20:56:02 +0200

    task 1

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python3
-""" Performs convolution on grayscale images"""
+"""
+Performs a valid convolution on grayscale images images
+"""
+
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
-    """Function to convolve the image"""
-    if images.ndim == 4:
-        m, h, w, _ = images.shape
-        # Convert RGB to grayscale
-        images = (0.2989 * images[:, :, :, 0] +
-                  0.5870 * images[:, :, :, 1] +
-                  0.1140 * images[:, :, :, 2])
-    else:
-        m, h, w = images.shape
+    """ convolve grayscale images
 
+    Args:
+        images (_type_): _description_
+        kernel (_type_): _description_
+    """
+
+    m, h, w = images.shape
     kh, kw = kernel.shape
-
-    # Calculate new dimensions after VALID convolution
-    new_h = h - kh + 1
-    new_w = w - kw + 1
-
-    # Initialize the array for convolved images
-    output = np.zeros((m, new_h, new_w))
-
-    # Perform the convolution
-    for i in range(new_h):
-        for j in range(new_w):
-            sub_matrix = images[:, i: i + kh, j: j + kw] * kernel
-            output[:, i, j] = np.sum(sub_matrix, axis=(1, 2))
+    output_h = h - kh + 1
+    output_w = w - kw + 1
+    output = np.zeros((m, output_h, output_w))
+    for i in range(output_h):
+        for j in range(output_w):
+            output[:, i, j] = (kernel * images[:, i: i + kh, j: j + kw])\
+                .sum(axis=(1, 2))
     return output
