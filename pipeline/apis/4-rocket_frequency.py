c18e7b6df4c597ef2984b474f93a951c7e8091e6
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2024-03-09 17:40:38 +0200

    Update 4-rocket_frequency.py

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -9,29 +9,23 @@ if rockets have the same amount of launches, in alphabetical order
 import requests
 
 def get_rocket_name(rocket_id):
-    url = f'https://api.spacexdata.com/v4/rockets/{rocket_id}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json().get('name', 'Unknown Rocket')
-    return 'Unknown Rocket'
+    rocket_url = 'https://api.spacexdata.com/v4/rockets/{}'.format(rocket_id)
+    response = requests.get(rocket_url).json()
+    return response.get('name')
 
 if __name__ == "__main__":
-    url = 'https://api.spacexdata.com/v4/launches'
-    launches = requests.get(url).json()
-    
-    rocket_counts = {}
+    launches_url = 'https://api.spacexdata.com/v4/launches'
+    launches = requests.get(launches_url).json()
+    rocket_frequency = {}
+
     for launch in launches:
         rocket_id = launch.get('rocket')
-        rocket_counts[rocket_id] = rocket_counts.get(rocket_id, 0) + 1
-
-    # Fetch rocket names
-    rocket_names = {}
-    for rocket_id in rocket_counts:
         rocket_name = get_rocket_name(rocket_id)
-        rocket_names[rocket_name] = rocket_counts[rocket_id]
 
-    # Sort by number of launches and then alphabetically
-    sorted_rockets = sorted(rocket_names.items(), key=lambda x: (-x[1], x[0]))
+        if rocket_name:
+            rocket_frequency[rocket_name] = rocket_frequency.get(rocket_name, 0) + 1
+
+    sorted_rockets = sorted(rocket_frequency.items(), key=lambda x: (-x[1], x[0]))
 
     for rocket in sorted_rockets:
-        print(f"{rocket[0]}: {rocket[1]}")
+        print("{}: {}".format(rocket[0], rocket[1]))
