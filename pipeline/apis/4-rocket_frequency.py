1200fbf6f9f255627371aebabe427d93a245b703
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2024-03-09 16:37:49 +0200

    Update 4-rocket_frequency.py

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
-"""
-Uses the (unofficial) SpaceX API to print the number of launches per rocket as:
-<rocket name>: <number of launches>
-ordered by the number of launches in descending order or,
-if rockets have the same amount of launches, in alphabetical order
-"""
-
-
+""" Script that displays the number of launches per rocket"""
 import requests
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    object = dict()
     url = 'https://api.spacexdata.com/v4/launches'
-    results = requests.get(url).json()
-    rocketDict = {}
-    for launch in results:
-        rocket = launch.get('rocket')
-        url = 'https://api.spacexdata.com/v4/rockets/{}'.format(rocket)
-        results = requests.get(url).json()
-        rocket = results.get('name')
-        if rocketDict.get(rocket) is None:
-            rocketDict[rocket] = 1
+    launches = requests.get(url).json()
+    for launch in launches:
+        urls = "https://api.spacexdata.com/v4/rockets/{}"
+        rocket_id = launch['rocket']
+        rocket_url = urls.format(rocket_id)
+        rocket_name = requests.get(rocket_url).json()['name']
+
+        if rocket_name in object.keys():
+            object[rocket_name] += 1
         else:
-            rocketDict[rocket] += 1
-    rocketList = sorted(rocketDict.items(), key=lambda kv: kv[0])
-    rocketList = sorted(rocketList, key=lambda kv: kv[1], reverse=True)
-    for rocket in rocketList:
-        print("{}: {}".format(rocket[0], rocket[1]))
+            object[rocket_name] = 1
+
+    keys = sorted(object.items(), key=lambda x: x[0])
+    keys = sorted(keys, key=lambda x: x[1], reverse=True)
+
+    for k in keys:
+        print("{}: {}".format(k[0], k[1]))
+        
