635f9518c68bcb0c347ca620eb12744a576d1358
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2024-03-09 17:31:46 +0200

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
-""" Script for getting user location"""
-import sys
+
+"""
+This module contains a function that
+uses Github API to print location of
+specific users"""
+
 import requests
-import time
+import sys
+
+# get the url from string passed on the terminal
+
+# If the user doesn’t exist, print Not found
+# If the status code is 403, print Reset in X min
+# where X is the number of minutes from now and the value of X-Ratelimit-Reset
+# Your code should not be executed when the file is imported
+# (you should use if __name__ == '__main__':)
 
 
-if __name__ == '__main__':
+def print_location():
+    """print location of user"""
     url = sys.argv[1]
     response = requests.get(url)
-    res = response.json()
+    data = response.json()
 
     if response.status_code == 200:
-        print(res['location'])
-    elif response.status_code == 404:
-        print('Not found')
+        if 'location' in data and data['location']:
+            print(data['location'])
+        else:
+            print("Not found")
     elif response.status_code == 403:
-        limit = int(response.headers['X-Ratelimit-Reset'])
-        start = int(time.time())
-        elapsed = int((limit - start) / 60)
-        print('Reset in {} min'.format(int(elapsed)))
+        print("Reset in {} min".format(
+            (int(response.headers['X-Ratelimit-Reset']) -
+             int(response.headers['X-Ratelimit-Reset'])) / 60))
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    print_location()
